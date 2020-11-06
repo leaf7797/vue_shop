53cb4fcb8c4ca212c82429e0dae7e07e2d0ad565
Yezi <leaf77@gwu.edu>
2020-11-06 14:27:20 -0500

    users completed

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -40,7 +40,7 @@
                         <!-- 修改按钮 -->
                         <el-button type="primary" icon="el-icon-edit" size="mini" @click="showEditDialog(scope.row.id)"></el-button>
                         <!-- 删除按钮 -->
-                        <el-button type="danger" icon="el-icon-delete" size="mini" ></el-button>
+                        <el-button type="danger" icon="el-icon-delete" size="mini" @click="removeUserById(scope.row.id)"></el-button>
                         <!-- 分配角色按钮 -->
                             <el-tooltip effect="dark" content="分配角色" placement="top" :enterable = "false">
                                 <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
@@ -296,6 +296,31 @@ export default {
                 // 提示修改成功
                 this.$message.success('更新用户信息成功！')
             })
+        },
+        async removeUserById(id){
+            // console.log(Object)
+            // 弹框询问用户是否删除数据
+            const confirmResult = await this.$confirm('此操作将永久删除该用户, 是否继续?', '提示', {
+            confirmButtonText: '确定',
+            cancelButtonText: '取消',
+            type: 'warning'
+        }).catch(err => {
+            return err
+        })
+
+        // 如果用户确认删除，则返回值为字符串confirm
+        // 如果用户取消了删除，则返回值为字符串cancel
+        // console.log(confirmResult)
+        if(confirmResult !== 'confirm'){
+            return this.$message.info('已取消删除')
+        }
+
+        const{data:res} = await this.$http.delete('users/'+id)
+        if(res.meta.status !== 200){
+            return this.$message.error('删除用户失败！')
+        }
+        this.$message.success('删除用户成功！')
+        this.getUserList()
         }
     }
 }
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -22,7 +22,8 @@ import {
     Switch,
     Tooltip,
     Pagination,
-    Dialog
+    Dialog,
+    MessageBox
 
 } from 'element-ui'
 
@@ -49,4 +50,6 @@ Vue.use(Tooltip)
 Vue.use(Pagination)
 Vue.use(Dialog)
 
+
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
