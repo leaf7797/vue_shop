64aba1de4c8d8d55c057bd05d89a4adbe160284b
Yezi <leaf77@gwu.edu>
2020-11-09 16:22:23 -0500

    rights completed

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -25,7 +25,10 @@ import {
     Dialog,
     MessageBox,
     Tag,
-    Tree
+    Tree,
+    Select,
+    Option
+
 
 } from 'element-ui'
 
@@ -53,6 +56,8 @@ Vue.use(Pagination)
 Vue.use(Dialog)
 Vue.use(Tag)
 Vue.use(Tree)
+Vue.use(Select)
+Vue.use(Option)
 
 
 Vue.prototype.$message = Message
