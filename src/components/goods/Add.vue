7c4ae47770005af4921087ae447bb4740b7a7ad3
Yezi <leaf77@gwu.edu>
2020-11-17 21:54:51 -0500

    add goods completed

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -76,18 +76,37 @@
                             :action= "uploadURL"
                             :on-preview="handlePreview"
                             :on-remove="handleRemove"
-                            list-type="picture">
+                            list-type="picture"
+                            :headers="headerObj"
+                            :on-success="handleSuccess">
                             <el-button size="small" type="primary">点击上传</el-button>
                         </el-upload>
                     </el-tab-pane>
-                    <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
+                    <el-tab-pane label="商品内容" name="4">
+                        <!-- 富文本编辑器组件 -->
+                        <quill-editor v-model="addForm.goods_introduce"></quill-editor>
+
+                        <!-- 添加商品的按钮 -->
+                        <el-button type="primary" class="btnAdd" @click="add">添加商品</el-button>
+                    </el-tab-pane>
                 </el-tabs>
             </el-form>
         </el-card>
+
+
+        <!-- 图片预览 -->
+        <el-dialog
+            title="图片预览"
+            :visible.sync="previewVisible"
+            width="50%">
+            <img :src="previewPath" alt="" class="previewImg">
+        </el-dialog>
     </div> 
 </template>
 
 <script>
+import _ from "lodash"
+
 export default {
     data(){
         return{
@@ -99,7 +118,12 @@ export default {
                 goods_weight:0,
                 goods_number:0,
                 // 商品所属的分类数组
-                goods_cat:[]
+                goods_cat:[],
+                // 图片数组
+                pics:[],
+                // 商品的详情描述
+                goods_introduce:'',
+                attrs:[]
             },
             addFormRules:{
                 goods_name:[
@@ -130,7 +154,13 @@ export default {
             // 静态属性列表数据
             onlyTableData:[],
             // 上传图片的URL地址
-            uploadURL:'http://127.0.0.1:8888/api/private/v1/upload'
+            uploadURL:'http://127.0.0.1:8888/api/private/v1/upload',
+            // 图片上传组件的headers请求头对象
+            headerObj:{
+                Authorization: window.sessionStorage.getItem('token')
+            },
+            previewPath:'',
+            previewVisible: false
         }
     }, 
     created(){
@@ -187,13 +217,71 @@ export default {
             }
         },
         // 处理图片预览效果
-        handlePreview(){
-
+        handlePreview(file){
+           this. previewPath = file.response.data.url
+           this.previewVisible = true
         },
         // 处理移除图片的操作
-        handleRemove(){
+        handleRemove(file){
+            // 1. 获取将要删除的图片的临时路径
+            // 2. 从pics数组中，找到这个图片对应的索引值
+            // 3. 调用数组的splice方法，把图片信息对象，从pics数组中移除
+            const filePath = file.response.data.tmp_path
+            const i = this.addForm.pics.findIndex(x => x.pic === filePath)
+            this.addForm.pics.splice(i, 1)
+            console.log(this.addForm)
+        },
+        // 监听图片上传成功的事件
+        // 监听图片上传成功的事件
+        handleSuccess(response) {
+            console.log(response)
+            // 1. 拼接得到一个图片信息对象
+            const picInfo = { pic: response.data.tmp_path }
+            // 2. 将图片信息对象，push 到pics数组中
+            this.addForm.pics.push(picInfo)
+            console.log(this.addForm)
+        },
+        add(){
+            this.$refs.addFormRef.validate(async valid => {
+               if(!valid){
+                    return this.$message.error('请填写必要的表单项！')
+               }
+            //执行添加的业务逻辑
+            // lodash  cloneDeep（obj)
+            const form = _.cloneDeep(this.addForm)
+            form.goods_cat = form.goods_cat.join(',')
 
+            // 处理动态参数
+            this.manyTableData.forEach(item => {
+                const newInfo = {attr_id : item.attr_id,
+                attr_value : item.attr_vals.join(' ') 
+                }
+                this.addForm.attrs.push(newInfo)
+            })
+            // 处理动态属性
+            this.onlyTableData.forEach(item => {
+                const newInfo = {
+                    attr_id : item.attr_id,
+                    attr_value : item.attr_vals
+                }
+                this.addForm.attrs.push(newInfo)
+            })
+            form.attrs = this.addForm.attrs
+            console.log(form)
+
+            // 发起请求添加商品
+            // 商品的名称，必须是唯一的
+            const {data : res} = await this.$http.post('goods', form)
+
+            if(res.meta.status !== 201){
+                return this.$message.error('添加商品失败！')
+            }
+            this.$message.success('添加商品失败！')
+            this.$router.push('/goods')
+
+            })
         }
+        
     },
     computed:{
         cateId(){
@@ -213,6 +301,14 @@ export default {
 
 .el-checkbox{
     margin: 0 10px 0 0 !important;
+}
+
+.previewImg{
+    width: 100%;
+}
+
+.btnAdd{
+    margin-top: 15px;
 }
 </style>
 
--- src/main.js
+++ src/main.js
@@ -12,6 +12,13 @@ import './assets/fonts/iconfont.css'
 import './assets/css/global.css'
 import TreeTable from 'vue-table-with-tree-grid'
 
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+// require styles 导入富文本编辑器对应的样式
+import 'quill/dist/quill.core.css'
+import 'quill/dist/quill.snow.css'
+import 'quill/dist/quill.bubble.css'
+
 import axios from 'axios'
 // 配置请求的跟路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
@@ -27,6 +34,11 @@ Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 Vue.component('tree-table', TreeTable)
+
+// 将富文本编辑器，注册为全局可用的组件
+Vue.use(VueQuillEditor);
+
+
 Vue.filter('dateFormat', function(originVal) {
     const dt = new Date(originVal)
 
