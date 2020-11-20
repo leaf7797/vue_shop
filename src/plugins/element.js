9b03f2fd50b85328b704eb66450a360173fa09b5
Yezi <leaf77@gwu.edu>
2020-11-20 16:17:25 -0500

    order completed

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -36,8 +36,13 @@ import {
     CheckboxGroup,
     Checkbox,
     Upload
+    // Timeline,
+    // TimelineItem
 
 } from 'element-ui'
+
+import Timeline from './timeline/index.js'
+import TimelineItem from './timeline-item/index.js'
 
 Vue.use(Button)
 Vue.use(Form)
@@ -73,6 +78,8 @@ Vue.use(Step)
 Vue.use(CheckboxGroup)
 Vue.use(Checkbox)
 Vue.use(Upload)
+Vue.use(Timeline)
+Vue.use(TimelineItem)
 
 
 Vue.prototype.$confirm = MessageBox.confirm
